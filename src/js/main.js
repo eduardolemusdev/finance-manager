e8f94e840494063a64791eef277f3b183d2fdbd1
Eduardo Lemus <eduardolemus3098@gmail.com>
2024-10-09 21:53:04 -0600

    feat: add transacction secction created and display responsive

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const selectTransaction = document.getElementById("transaction-select");
+const displaySelectTransactionButton = document.getElementById("selectButton");
+const transactionSelectDropdown = document.getElementById(
+  "transaction-select_dropdown"
+);
