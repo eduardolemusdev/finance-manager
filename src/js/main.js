99d66b2b7747f417d9ddbebd821dc67e2c73dc9c
Eduardo Lemus <eduardolemus3098@gmail.com>
2024-10-12 07:28:21 -0600

    feat: animation of transacttions tab created

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,61 @@
-const selectTransaction = document.getElementById("transaction-select");
-const displaySelectTransactionButton = document.getElementById("selectButton");
-const transactionSelectDropdown = document.getElementById(
-  "transaction-select_dropdown"
-);
+const ingressTabButton = document.getElementById("ingresTabButton");
+const egressTabButton = document.getElementById("egressTabButton");
+const currentTabButton = document.getElementById("currentTabBackground");
+
+const activeAttribute = "active-button";
+const transacctionsArrayMock = [
+  {
+    transaccionId: 1,
+    descripcion: "Pago de renta",
+    monto: 500.0,
+    type: "egress",
+  },
+  {
+    transaccionId: 2,
+    descripcion: "Compra en supermercado",
+    monto: 120.5,
+    type: "egress",
+  },
+  {
+    transaccionId: 3,
+    descripcion: "Recarga de celular",
+    monto: 10.0,
+    type: "egress",
+  },
+  {
+    transaccionId: 4,
+    descripcion: "Sueldo recibido",
+    monto: 1500.0,
+    type: "ingress",
+  },
+  {
+    transaccionId: 5,
+    descripcion: "Transferencia recibida",
+    monto: 200.0,
+    type: "ingress",
+  },
+];
+
+const listTransacctionsByType = (type) => {};
+
+ingressTabButton.addEventListener("click", () => {
+  const isActive = egressTabButton.getAttribute(activeAttribute) === "1";
+  if (isActive) {
+    currentTabButton.classList.add("fromEgressButton");
+    currentTabButton.classList.remove("fromIngressButton");
+
+    ingressTabButton.setAttribute(activeAttribute, "1");
+    egressTabButton.removeAttribute(activeAttribute);
+  }
+});
+
+egressTabButton.addEventListener("click", () => {
+  const isActive = ingressTabButton.getAttribute(activeAttribute) === "1";
+  if (isActive) {
+    currentTabButton.classList.add("fromIngressButton");
+    currentTabButton.classList.remove("fromEgressButton");
+
+    egressTabButton.setAttribute(activeAttribute, "1");
+    ingressTabButton.removeAttribute(activeAttribute);
+  }
+});
